3687d572d3aadc3141d819469b4a058b0b014037
Andres Sewell <sewellandres@gmail.com>
2024-05-31 21:05:58 -0500

    End of Hello Triangle

---
+++ build/CMakeFiles/learnopengl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learnopengl.dir/main.cpp.o"
+  "CMakeFiles/learnopengl.dir/main.cpp.o.d"
+  "learnopengl"
+  "learnopengl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learnopengl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
